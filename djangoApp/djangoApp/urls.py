834ad0c0b5843bcb1e786cf9f51ecc282b08fcd4
ikkyu0714 <ikkyu1955@gmail.com>
2023-02-21 17:48:28 +0900

    hoge/urls.pyでhoge/のルーティングを設定

--- djangoApp/djangoApp/urls.py
+++ djangoApp/djangoApp/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('hoge', views.hoge),
+    path('hoge/', include('hoge.urls')),
+    path('fuga', views.fuga),
     path('admin/', admin.site.urls)
 ]
--- djangoApp/djangoApp/views.py
+++ djangoApp/djangoApp/views.py
@@ -2,3 +2,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World")
+
+def hoge(request):
+    return HttpResponse("Hoge")
+
+def fuga(request):
+    return HttpResponse("Fuga")
