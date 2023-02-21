4fe3157fce295be8a4421ca2596ffda42645bb25
ikkyu0714 <ikkyu1955@gmail.com>
2023-02-21 18:15:31 +0900

    静的ファイルの送信 static/style.cssを反映する

--- djangoApp/djangoApp/views.py
+++ djangoApp/djangoApp/views.py
@@ -1,7 +1,19 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello World")
+    #return HttpResponse("Hello World")
+    return HttpResponse("""
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8"/>
+                <link rel="stylesheet" href="/static/style.css"/>
+        </head>
+        <body>
+        <h1>Hello World</h1>
+        </body>
+    </html>
+    """)
 
 def hoge(request):
     return HttpResponse("Hoge")
